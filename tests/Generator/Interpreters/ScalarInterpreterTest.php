8de382d883fe31f394c3a2c0a1c1a4971321de3d
Marc-Andre Boily <maboily@kronostechnologies.com>
2017-02-20 10:12:55 -0500

    Put back deleted unit tests

---
+++ tests/Generator/Interpreters/ScalarInterpreterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace GraphQLGen\Tests\Generator\Interpreters;
+
+
+use GraphQL\Language\AST\NameNode;
+use GraphQL\Language\AST\ScalarTypeDefinitionNode;
+use GraphQLGen\Generator\Interpreters\ScalarInterpreter;
+
+class ScalarInterpreterTest extends \PHPUnit_Framework_TestCase {
+	const VALID_DESCRIPTION = 'TestDescription';
+	const VALID_NAME = 'TestName';
+
+	public function test_GivenNodeWithName_getName_ReturnsCorrectName() {
+		$scalarNode = new ScalarTypeDefinitionNode([]);
+		$this->GivenNodeWithName($scalarNode);
+
+		$interpreter = new ScalarInterpreter($scalarNode);
+		$interpretedName = $interpreter->getName();
+
+		$this->assertEquals(self::VALID_NAME, $interpretedName);
+	}
+
+	public function test_GivenNodeWithDescription_getDescription_ReturnsCorrectDescription() {
+		$scalarNode = new ScalarTypeDefinitionNode([]);
+		$this->GivenNodeWithDescription($scalarNode);
+
+		$interpreter = new ScalarInterpreter($scalarNode);
+		$interpretedDescription = $interpreter->getDescription();
+
+		$this->assertEquals(self::VALID_DESCRIPTION, $interpretedDescription);
+	}
+
+	protected function GivenNodeWithDescription($node) {
+		$node->description = self::VALID_DESCRIPTION;
+	}
+
+	protected function GivenNodeWithName($node) {
+		$node->name = new NameNode([]);
+		$node->name->value = self::VALID_NAME;
+	}
+}
