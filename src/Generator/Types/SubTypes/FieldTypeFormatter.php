07247a41fccb0a293511fb0c48b63e16bd5375f0
Marc-Andre Boily <maboily@kronostechnologies.com>
2017-02-22 08:48:09 -0500

    Split PSR4 field formatter logic

--- src/Generator/Types/SubTypes/FieldTypeFormatter.php
+++ src/Generator/Types/SubTypes/FieldTypeFormatter.php
@@ -10,6 +10,35 @@ abstract class FieldTypeFormatter {
 	 * @return string
 	 */
 	public function getFieldTypeDeclaration($fieldType) {
-		return null;
+		// Primary type check
+		if (in_array($fieldType->typeName, FieldType::$PRIMARY_TYPES_MAPPINGS)) {
+			$typeDeclaration = FieldType::$PRIMARY_TYPES_MAPPINGS[$fieldType->typeName];
+		}
+		else {
+			$typeDeclaration = $this->getFieldTypeDeclarationNonPrimaryType($fieldType);
+		}
+
+		// Is base object nullable?
+		if(!$fieldType->isTypeNullable) {
+			$typeDeclaration = 'Type::nonNull(' . $typeDeclaration . ')';
+		}
+
+		// Is in list?
+		if($fieldType->inList) {
+			$typeDeclaration = 'Type::listOf(' . $typeDeclaration . ')';
+		}
+
+		// Is list nullable?
+		if($fieldType->inList && !$fieldType->isListNullable) {
+			$typeDeclaration = 'Type::nonNull(' . $typeDeclaration . ')';
+		}
+
+		return $typeDeclaration;
 	}
+
+	/**
+	 * @param FieldType $fieldType
+	 * @return string
+	 */
+	abstract public function getFieldTypeDeclarationNonPrimaryType($fieldType);
 }
--- src/Generator/Writer/PSR4/PSR4FieldTypeFormatter.php
+++ src/Generator/Writer/PSR4/PSR4FieldTypeFormatter.php
@@ -12,30 +12,7 @@ class PSR4FieldTypeFormatter extends FieldTypeFormatter {
 	 * @param FieldType $fieldType
 	 * @return string
 	 */
-	public function getFieldTypeDeclaration($fieldType) {
-		// Primary type check
-		if (in_array($fieldType->typeName, FieldType::$PRIMARY_TYPES_MAPPINGS)) {
-			$typeDeclaration = FieldType::$PRIMARY_TYPES_MAPPINGS[$fieldType->typeName];
-		}
-		else {
-			$typeDeclaration = 'TypeStore::get' . $fieldType->typeName . '()';
-		}
-
-		// Is base object nullable?
-		if(!$fieldType->isTypeNullable) {
-			$typeDeclaration = 'Type::nonNull(' . $typeDeclaration . ')';
-		}
-
-		// Is in list?
-		if($fieldType->inList) {
-			$typeDeclaration = 'Type::listOf(' . $typeDeclaration . ')';
-		}
-
-		// Is list nullable?
-		if($fieldType->inList && !$fieldType->isListNullable) {
-			$typeDeclaration = 'Type::nonNull(' . $typeDeclaration . ')';
-		}
-
-		return $typeDeclaration;
+	public function getFieldTypeDeclarationNonPrimaryType($fieldType) {
+		return 'TypeStore::get' . $fieldType->typeName . '()';
 	}
 }
