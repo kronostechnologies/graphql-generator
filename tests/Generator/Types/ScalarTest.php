50cad8052a4733946c3c270bab24fe60df422663
Marc-Andre Boily <maboily@kronostechnologies.com>
2017-02-24 11:23:23 -0500

    Add ScalarTest

--- tests/Generator/Types/ScalarTest.php
+++ tests/Generator/Types/ScalarTest.php
@@ -4,20 +4,66 @@
 namespace GraphQLGen\Tests\Generator\Types;
 
 
+use GraphQLGen\Generator\Formatters\StubFormatter;
+use GraphQLGen\Generator\Types\Scalar;
+
 class ScalarTest extends \PHPUnit_Framework_TestCase {
-	public function test_GivenNamedScalarType_getName_WillReturnName() {
+	const VALID_NAME = 'ScalarDate';
+	const VALID_DESCRIPTION = 'This is a description for the scalar date type.';
 
+	public function test_GivenScalar_getName_WillReturnName() {
+		$scalar = $this->GivenScalar();
+
+		$retVal = $scalar->getName();
+
+		$this->assertEquals(self::VALID_NAME, $retVal);
 	}
 
-	public function test_GivenNamedScalarType_generateTypeDefinition_WillContainName() {
+	public function test_GivenScalar_generateTypeDefinition_WillContainName() {
+		$scalar = $this->GivenScalar();
 
+		$retVal = $scalar->generateTypeDefinition();
+
+		$this->assertContains(self::VALID_NAME, $retVal);
 	}
 
-	public function test_GivenNamedScalarType_getDependencies_WillBeEmpty() {
+	public function test_GivenScalarWithDescription_generateTypeDefinition_WillContainDescription() {
+		$scalar = $this->GivenScalarWithDescription();
 
+		$retVal = $scalar->generateTypeDefinition();
+
+		$this->assertContains(self::VALID_DESCRIPTION, $retVal);
 	}
 
-	public function test_GivenNamedScalarType_getConstants_WillReturnNull() {
+	public function test_GivenScalar_getDependencies_WillBeEmpty() {
+		$scalar = $this->GivenScalar();
 
+		$retVal = $scalar->getDependencies();
+
+		$this->assertEmpty($retVal);
+	}
+
+	public function test_GivenScalar_getConstantsDeclaration_WillReturnNull() {
+		$scalar = $this->GivenScalar();
+
+		$retVal = $scalar->getConstantsDeclaration();
+
+		$this->assertNull($retVal);
+	}
+
+	protected function GivenScalar() {
+		return new Scalar(
+			self::VALID_NAME,
+			new StubFormatter(),
+			null
+		);
+	}
+
+	protected function GivenScalarWithDescription() {
+		return new Scalar(
+			self::VALID_NAME,
+			new StubFormatter(),
+			self::VALID_DESCRIPTION
+		);
 	}
 }
