74c633e6d494d3c8fba94d2c42848ef0643155fc
Marc-Andre Boily <maboily@kronostechnologies.com>
2017-02-27 09:43:18 -0500

    Add PSR4StubFileTest

---
+++ tests/Generator/Writer/PSR4/PSR4StubFileTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace GraphQLGen\Tests\Generator\Writer\PSR4;
+
+
+use GraphQLGen\Generator\Writer\PSR4\PSR4StubFile;
+
+class PSR4StubFileTest extends \PHPUnit_Framework_TestCase {
+	const STUB_FILE_LINE_1 = "FirstLine;";
+	const STUB_FILE_LINE_2 = "namespace DummyNamespace;";
+	const STUB_FILE_LINE_3 = "class DummyClass {";
+	const STUB_FILE_LINE_4 = "'VariablesDeclarations';";
+
+	const NAMESPACE_NEW = "TestNamespace";
+	const CLASS_NEW = "NewClass";
+	const VARIABLES_DECLARATIONS = "var aVariable = 123;";
+
+	/**
+	 * @var PSR4StubFile
+	 */
+	protected $_givenStubFile;
+
+	public function setUp() {
+		$this->_givenStubFile = new PSR4StubFile();
+		$this->_givenStubFile->loadFromFile(getcwd() . '/tests/Mocks/Stubs/PSR4StubFileTest.stub');
+	}
+
+	public function test_GivenStubFile_getNamespaceDeclarationLine_WillReturnRightLine() {
+		$retVal = $this->_givenStubFile->getNamespaceDeclarationLine();
+
+		$this->assertEquals(self::STUB_FILE_LINE_2, $retVal);
+	}
+
+	public function test_GivenStubFile_getDummyClassNameLine_WillReturnRightLine() {
+		$retVal = $this->_givenStubFile->getDummyClassNameLine();
+
+		$this->assertEquals(self::STUB_FILE_LINE_3, $retVal);
+	}
+
+	public function test_GivenStubFile_getVariablesDeclarationLine_WillReturnRightLine() {
+		$retVal = $this->_givenStubFile->getVariablesDeclarationLine();
+
+		$this->assertEquals(self::STUB_FILE_LINE_4, $retVal);
+	}
+
+
+	public function test_GivenStubFile_writeNamespace_WillReplaceRightLine() {
+		$this->_givenStubFile->writeNamespace(self::NAMESPACE_NEW);
+		$retVal = $this->_givenStubFile->getContent();
+
+		$this->assertContains(self::NAMESPACE_NEW, $retVal);
+	}
+
+	public function test_GivenStubFile_writeClassName_WillReplaceRightLine() {
+		$this->_givenStubFile->writeClassName(self::CLASS_NEW);
+		$retVal = $this->_givenStubFile->getContent();
+
+		$this->assertContains(self::CLASS_NEW, $retVal);
+	}
+
+	public function test_GivenStubFile_writeVariablesDeclarations_WillReplaceRightLine() {
+		$this->_givenStubFile->writeVariablesDeclarations(self::VARIABLES_DECLARATIONS);
+		$retVal = $this->_givenStubFile->getContent();
+
+		$this->assertContains(self::VARIABLES_DECLARATIONS, $retVal);
+	}
+}
