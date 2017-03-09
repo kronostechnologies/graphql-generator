082641b23adf7c8976bdf3c7f1c0b5bdd2ca3084
Marc-Andre Boily <maboily@kronostechnologies.com>
2017-03-09 15:44:38 -0500

    Add PSR4WriterContext Test

---
+++ tests/Generator/Writer/PSR4/PSR4WriterContextTest.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace GraphQLGen\Tests\Generator\Writer\PSR4;
+
+
+use GraphQLGen\Generator\Formatters\StubFormatter;
+use GraphQLGen\Generator\Writer\PSR4\PSR4WriterContext;
+
+class PSR4WriterContextTest extends \PHPUnit_Framework_TestCase {
+	const FORMATTER_USE_SPACES = true;
+	const FORMATTER_TAB_SIZE = 8;
+	const STUB_FILE_NAME = 'AStubFile.stub';
+
+	/**
+	 * @var PSR4WriterContext
+	 */
+	protected $_writerContext;
+
+	public function setUp() {
+		$this->_writerContext = new PSR4WriterContext();
+	}
+
+	public function test_GivenStubFileName_getStubFilePath_WillContainStubDirectory() {
+		$retVal = $this->_writerContext->getStubFilePath(self::STUB_FILE_NAME);
+
+		$this->assertContains("/stubs/", $retVal);
+	}
+
+	public function test_GivenConfiguration_getConfiguredClassFormatter_WillConfigureClassFormatterCorrectly() {
+		$this->GivenConfiguration();
+
+		$retVal = $this->_writerContext->getConfiguredClassFormatter();
+
+		$this->assertEquals(self::FORMATTER_USE_SPACES, $retVal->usesSpaces());
+		$this->assertEquals(self::FORMATTER_TAB_SIZE, $retVal->getTabSize());
+	}
+
+	protected function GivenConfiguration() {
+		$this->_writerContext->formatter = new StubFormatter();
+		$this->_writerContext->formatter->useSpaces = self::FORMATTER_USE_SPACES;
+		$this->_writerContext->formatter->tabSize = self::FORMATTER_TAB_SIZE;
+	}
+}
