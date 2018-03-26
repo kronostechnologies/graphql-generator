bd2fe7ac3696b72ad400ccb7670ec947e271fd3b
Sylvain Lévesque <slevesque@kronostechnologies.com>
2018-03-26 10:42:22 -0400

    Add exit 0.

--- bin/build.sh
+++ bin/build.sh
@@ -8,3 +8,5 @@ NAMESPACE=Kronos\\CRM\\GraphQL\\Schema
 rm -rf "$DESTINATION"
 php $PWD/graphqlgen generate-classes -q --namespaced-target-namespace "$NAMESPACE" "$SCHEMA" "$DESTINATION"
 $PWD/vendor/bin/phpcbf -q --standard=$PWD/kronos_ruleset.xml "$DESTINATION"
+
+exit 0
